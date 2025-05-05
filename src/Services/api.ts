cc80f4929ef44e6da5749c1939449567756173dd
Geraldopereirads <geraldo.pereirads@hotmail.com>
2025-05-05 13:05:37 -0300

    update url

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://hyperion-api-4955.onrender.com/",
+  baseURL: "https://hyperion-api-hze8.onrender.com/",
   timeout: 15000,
 });
